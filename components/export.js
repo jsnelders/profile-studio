39c0ac11362bb3dc7dbd17569cbab6af42141282
chris2fr <chris@mann.fr>
2020-10-15 22:09:53 +0200

    Modified the date of the export

--- components/export.js
+++ components/export.js
@@ -5,6 +5,8 @@ var exportComponent = {
 
 	mounted: function()
 	{
+		stringDate = new Date().toISOString();
+		this.$root.sections.meta.lastModified = stringDate.slice(0,16) + stringDate.slice(stringDate.lastIndexOf("Z")); //.replace(":","");
 		this.json = JSON.stringify(this.$root.sections, null, 4);
 	},
 
